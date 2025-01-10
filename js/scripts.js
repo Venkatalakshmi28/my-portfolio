c78735440dc6eee79b8f937529611309f96f1b1a
Venkatalakshmi28 <dvenkateswaramma721@gmail.com>
2025-01-10 14:58:14 +0530

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,14 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
 
+    if (name && email && message) {
+        alert('Thank you for your message, ' + name + '!');
+        // Normally you would send the data to a server here
+    } else {
+        alert('Please fill out all fields.');
+    }
+});
